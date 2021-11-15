b67a0283b30b6c0c6405b45457363e95614cb9bc
dhanushpatel840 <91380702+dhanushpatel840@users.noreply.github.com>
2021-11-15 09:33:48 +0530

    Add files via upload

---
+++ Pangram.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Pangram {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String str = in.nextLine();
+
+        int count[] = new int[26];
+        boolean flag = true;
+        char ch;
+
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+
+            if (ch == ' ')
+                continue;
+
+            if (ch >= 'A' && ch <= 'Z') {
+                count[ch - 'A']++;
+            } else if (ch >= 'a' && ch <= 'z') {
+                count[ch - 'a']++;
+            }
+
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            System.out.println("Pangram String");
+        else
+            System.out.println("Not a Pangram String");
+
+    }
+}
